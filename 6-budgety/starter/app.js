35ae59b50d5732a2812afa3fd83b3f8a81d14945
Serdar Mustafa <serdarmustafa@gmail.com>
2019-03-17 15:04:42 +0200

    app init added

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -33,7 +33,22 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMStrings();
+
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+
+            if (event.keyCode === 13) {
+                ctrlAddItem();
+            }
+
+        });
+    };
+
+
 
     var ctrlAddItem = function() {
 
@@ -47,18 +62,18 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         //4. Calculate the budget
 
-        //5. Display the budget on the UI		
+        //5. Display the budget on the UI        
 
+    };
+
+return {
+    init: function() {
+        console.log('App started');
+        setupEventListeners();
     }
+};
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-
-        if (event.keyCode === 13) {
-            ctrlAddItem();
-        }
-
-    });
 
 })(BudgetController, UIController);
+
+controller.init();
